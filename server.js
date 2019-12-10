56fc73baca3f732e1921edd2562d67d7e2885510
AliStoltz <ali.mstoltz@gmail.com>
2019-12-10 11:47:05 -0800

    backend heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const routes = require('./routes');
 
 // CORS 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000',  'https://guitar-hub-frontend.herokuapp.com'],
   credentials: true,
   optionsSuccessStatus: 200
 };
